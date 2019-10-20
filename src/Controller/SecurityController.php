8cd21388c3cdd7d73f2148e9fb82021e36115864
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-19 20:01:12 -0400

    add basic registration form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use App\Security\LoginFormAuthenticator;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
@@ -30,5 +35,38 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
+    {
+        if ($request->isMethod('POST')) {
+            $user = new User();
+            $user->setUsername($request->request->get('username'));
+            $user->setFirstname('Mystery');
+            $user->setlastname('Mystery');
+            $user->setCountry('Mystery');
+            $user->setCity('Mystery');
+            $user->setEmail('Mystery@mail.fr');
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $request->request->get('password')
+            ));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+            );
+        }
+
+        return $this->render('security/register.html.twig');
     }
 }
