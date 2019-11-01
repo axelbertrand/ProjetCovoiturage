5afe0c03cbf0ab2c02d5531f2394369eaf6881da
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-31 21:45:49 -0400

    create basic form for departure publications

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\DeparturePublicationFormType;
 
 class PublicationController extends AbstractController
 {
@@ -12,8 +13,10 @@ class PublicationController extends AbstractController
      */
     public function index()
     {
+        $form = $this->createForm(DeparturePublicationFormType::class);
+
         return $this->render('publication/index.html.twig', [
-            
+            'departurePublicationForm' => $form->createView()
         ]);
     }
 }
