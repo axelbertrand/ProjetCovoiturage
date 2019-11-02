87eeeffe36bf606e9508918c9d4779885b1d9596
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-11-01 22:29:48 -0400

    make user(email) unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -56,7 +56,7 @@ class User implements UserInterface
     private $city;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $email;
 
